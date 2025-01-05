7bec7db39f77df19b41fe8da524df585fcb1090e
Navaneeth Tripathi Aman <142510994+aman-tripathi-navaneeth@users.noreply.github.com>
2025-01-05 20:23:31 +0530

    Create 2nd_program.c

---
+++ 2nd_program.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main() 
+{
+    char str[] = "Hello World";
+    char str1[11];
+    char str2[11];
+    char str3[11];
+    int i, len;
+
+    len = strlen(str); // Get the length of the string
+
+    // Copy str to str2
+    strcpy(str2, str);
+
+    // First loop: Perform bitwise AND with 127
+    for (i = 0; i < len; i++) 
+    {
+        str1[i] = str[i] & 127;
+        printf("%c", str1[i]);
+    }
+    printf("\n");
+
+    // Second loop: Perform bitwise XOR with 127
+    for (i = 0; i < len; i++) 
+    {
+        str3[i] = str2[i] ^ 127;
+        printf("%c", str3[i]);
+    }
+    printf("\n");
+}
+
+// Output:
+// Hello World
+// Hello World
+// Hello World
