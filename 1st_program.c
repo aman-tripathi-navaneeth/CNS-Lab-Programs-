ca93a8c6ce76be6995a5204745810e0d7b73d121
Navaneeth Tripathi Aman <142510994+aman-tripathi-navaneeth@users.noreply.github.com>
2025-01-06 18:53:57 +0530

    1st_program.c

--- 1st_program.c
+++ 1st_program.c
@@ -1,22 +1,18 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-int main() 
+#include<stdlib.h> 
+main() 
 { 
-    char str[] = "Hello World"; 
-    char str1[12]; // Increased size to accommodate null terminator
-    int i, len; 
-    len = strlen(str); 
-    for (i = 0; i < len; i++) 
-    { 
-        str1[i] = str[i]; 
-        printf("%c", str1[i]); 
-    } 
-    str1[len] = '\0'; // Null-terminate the string
-    printf("\n%s\n", str1); // Print the copied string
-    return 0; 
+char str[]="Hello World"; 
+char str1[11]; 
+int i,len; 
+len=strlen(str); 
+for(i=0;i<len;i++) 
+{ 
+str1[i]=str[i]^0; 
+printf("%c",str1[i]); 
 } 
-// Output:
-// Hello World
-//
+printf("\n"); 
+}
+
+// output:-
+// hello world
+// hello world
